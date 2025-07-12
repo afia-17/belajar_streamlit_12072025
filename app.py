9cb6fb47088c14c537d155e0ccf0c67d5d410fc4
afia-17 <afiahkmwt@gmail.com>
2025-07-12 09:24:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
 
+import time
 with st.sidebar:
     with st.echo():
         st.write("This code will be printed to the sidebar.")
